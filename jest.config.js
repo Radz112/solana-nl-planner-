18f553f3b7b430687369e2e6290781a20040d6e4
Radz112 <radlin@magpiexyz.io>
2026-02-06 08:47:09 +0200

    Phase 0: Project scaffolding

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+};
